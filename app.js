fc08bc6101ad1337f2bedb24dc79a1c34088f6d3
MINJU-KIMmm <0112km@ewhain.net>
2022-09-14 23:18:07 +0900

    2.6 Objects

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-//boolean
+//Objects
 
-const amIFat = null;
-let something;
-console.log(amiFat); //null : 변수 안에 어떤 것이 없다는 것을 의도적으로 명시한 것
-console.log(something) //undefined : 메모리 안에 값이 없는 것
+
+//list
+const player = ["nico", 1212, false, "little bit"];
+
+//Object
+const obPlayer = {
+    name : "nico",
+    points : 10,
+    fat : true,
+}
+
+obPlayer.fat = false; // object 안의 값을 수정하는 것은 가능. object 전체를 수정하는 것은 불가능(obPlayer = ~~)
+
+obPlayer.lastName = "potato";// object에 값 추가
+
+console.log(obPlayer);
+console.log(obPlayer.name);
