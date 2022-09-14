1bc94993c595447ddf9588973d36542609efca16
MINJU-KIMmm <0112km@ewhain.net>
2022-09-14 23:06:40 +0900

    2.3 const and let

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const a = 5; //변수
-const b = 2
+//변수 : let or const
+// const : 상수. 바뀔 수 없음
+// let : 바꿀 수 있음
+const a = 5; 
+const b = 2 
 
-const myName = "nico";
+let myName = "nico"; //js는 보통 카멜체
+const veryLongVariableName = 0;
 
 console.log(a+b); //console에 출력
 console.log(a * b);
 console.log(a / b);
+
+console.log("hello" + myName); //string +도 가능
+
+myName = "haha";
+
+console.log("your name is " + myName)
