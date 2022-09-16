789cf0d8ee482cf984e219fccff28a88415f9c78
MINJU-KIMmm <0112km@ewhain.net>
2022-09-16 15:46:39 +0900

    2.13 Conditionals 3

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const age = parseInt(prompt("How old are you?")); //prompt : 사용자에게 창
 
 //console.log(isNaN(age)); //숫자가 아닌지
 
-if(isNaN(age)) {
-    console.log("Please write a number.")
-} else {
-    console.log("Thank you!")
+if(isNaN(age) || age < 0) {
+    console.log("Please write a positive number.")
+} else if(age < 18) {
+    console.log("You are too young");
+} else if(age>=18 && age <= 50) { //and : && , or : ||
+    console.log("You can drink.")
+} else if(age > 50 && age <= 80) {
+    console.log("You should exercise.")
+} else if (age > 80) {
+    console.log("You can do whatever");
 }
