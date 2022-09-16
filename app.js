e94c51574532df53a24a1727826c9ad27fbfbcbc
MINJU-KIMmm <0112km@ewhain.net>
2022-09-16 15:58:08 +0900

    3.0 The Document Object

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-//2.13 Conditionals
+//3.0 The Document Object
 
-const age = parseInt(prompt("How old are you?")); //prompt : 사용자에게 창을 띄워줌. prompt(message(string), default)
+// js : html의 element들을 js로 변경, 읽음
 
-//console.log(isNaN(age)); //숫자가 아닌지
-
-if(isNaN(age) || age < 0) {
-    console.log("Please write a positive number.")
-} else if(age < 18) {
-    console.log("You are too young");
-} else if(age>=18 && age <= 50) { //and : && , or : ||
-    console.log("You can drink.")
-} else if(age > 50 && age <= 80) {
-    console.log("You should exercise.")
-} else if (age > 80) {
-    console.log("You can do whatever");
-}
+//document : HTML 가져옴(object)
+// document.title -> HTML의 title 읽어옴
+// document.title = "hi" 이런식으로 html 변경도 가능
