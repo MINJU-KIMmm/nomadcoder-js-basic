f5f6ea8bf6bfbd7b8cc2719eb4a1d53263e5d9d8
MINJU-KIMmm <0112km@ewhain.net>
2022-09-24 21:53:16 +0900

    3.4 Events 2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//3.3 Events
+//3.4 Events 2
 
 const title = document.querySelector(".hello:first-child h1");
 
@@ -16,6 +16,16 @@ function handleTitleClick() {
     title.style.color = "red";
 }
 
-//실행시킬 function명에 괄호 넣으면 안됨
-title.addEventListener("click", handleTitleClick); //js에 어떤 event를 listen할지 알려주고 event 발생시 실행할 function 넘겨줌
+function handleMouseEnter() {
+    title.innerText = "mouse is here!";
+}
 
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
+//실행시킬 function명에 괄호 넣으면 안됨
+//console.dir 에서 on~~ 에서 on 빼고 ~~ 를 동작으로 넣어줌
+title.addEventListener("click", handleTitleClick); //js에 어떤 event를 listen할지 알려주고 event 발생시 실행할 function 넘겨줌
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
